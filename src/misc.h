3453953c468543fc9d193c2f919853ff8935b092
Batperson <batperson@github.com>
2018-08-28 22:43:07 +1200

    Added font resource

--- src/misc.h
+++ src/misc.h
@@ -7,6 +7,7 @@
 #define MISC_H_
 
 #define IN_CCM  			__attribute__((section(".ccmram")))
+#define IN_RAM  			__attribute__((section(".data")))
 #define ALIGNED(n) 			__attribute__((aligned(n)))
 #define INTERRUPT 			__attribute__((interrupt("IRQ")))
 #define ALWAYS_INLINE 		inline __attribute__((always_inline))
